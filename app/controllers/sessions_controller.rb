ee0292b270fdd0cdd8da7ad35170d85bed464b60
Jasmine Carter <jasmine.carter87@gmail.com>
2020-11-01 17:40:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    binding.pry
     if params[:name].empty?
       redirect_to(controller: 'sessions', action: 'new')
     else
